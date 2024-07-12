3838f880f8eeeab65751e0e55aee0f1665934a4c
SR Tamim <saifur.rahman.tamim1@gmail.com>
2024-07-12 12:39:02 +0600

    feat: Add algorithm to find maximum element in an array

---
+++ 1.Intro_to_algorithms/find-maximum-element-in-array.cpp
@@ -0,0 +1,32 @@
+/*
+Algorithm practice: finding maximum element in an array
+*/
+
+#include <iostream>
+using namespace std;
+
+int findMax(int arr[], int n)
+{
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Maximum element in the array is: " << findMax(arr, n) << endl;
+    return 0;
+}
